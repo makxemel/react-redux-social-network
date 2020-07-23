9ae06c43b216ae82b5c09af7c70d8e5e24fdeb8c
Maksim <delmst@yandex.ru>
2020-07-23 21:57:46 +0300

    rootReducer was typed

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,10 +5,10 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from "redux-form"
 import appReducer from "./app-reducer";
 
-let reducers = combineReducers({
+let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
@@ -18,8 +18,13 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers,  composeEnhancers(applyMiddleware(thunkMiddleware)));
-window.__store__ = store;
+type RootReducerType = typeof rootReducer; // (globalState: AppStateType) => AppStateType
+export type AppStateType = ReturnType<RootReducerType>
 
-export default store;
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer,  composeEnhancers(applyMiddleware(thunkMiddleware)))
+// @ts-ignore
+window.__store__ = store
+
+export default store
