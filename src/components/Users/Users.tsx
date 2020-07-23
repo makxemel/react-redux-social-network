b6e10548a6af92508417faa952cbb91756a0cc8a
Maksim <delmst@yandex.ru>
2020-07-23 22:01:14 +0300

    the component was typed

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Paginator from "../common/Paginator/Paginator";
+import User from "./User";
+import {UserType} from "../../types/types";
+
+type PropsType = {
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    onPageChanged: (pageNumber: number) => void
+    users: Array<UserType>
+    followingInProgress: Array<number>
+    unfollow: (userId: number) => void
+    follow: (userId: number) => void
+
+}
+
+let Users: React.FC<PropsType> = ({currentPage, totalUsersCount, pageSize, onPageChanged, users, followingInProgress, unfollow, follow}) => {
+    return <div>
+        <Paginator currentPage={currentPage} onPageChanged={onPageChanged}
+                   totalItemsCount={totalUsersCount} pageSize={pageSize}/>
+        <div>
+            {
+                users.map(u => <User user={u}
+                                     followingInProgress={followingInProgress}
+                                     key={u.id}
+                                     unfollow={unfollow}
+                                     follow={follow}
+                    />
+                )
+            }
+        </div>
+    </div>
+}
+
+export default Users;
